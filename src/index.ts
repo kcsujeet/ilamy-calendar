7a04950162239effcae93f92396ca24b52bf9975
Sujeet Kc <sujeetkc45@gmail.com>
2025-07-19 14:01:08 -0300

    feat: add bunup configuration and update package.json for build process

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+// Main calendar component
+export { IlamyCalendar } from './components/ilamy-calendar/ilamy-calendar'
+
+// Public calendar context hook
+export { usePublicCalendarContext as useIlamyCalendarContext } from './contexts/calendar-context/context'

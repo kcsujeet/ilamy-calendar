cf5982d77baba3392da539163244f02107d9b278
Sujeet Kc <sujeetkc45@gmail.com>
2025-07-20 10:36:17 -0300

    fix: correct DAY_NUMBER_HEIGHT constant to match design specifications

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,3 @@
 export const GAP_BETWEEN_ELEMENTS = 1 // px (gap-1)
-export const DAY_NUMBER_HEIGHT = 24 // px (h-6)
+export const DAY_NUMBER_HEIGHT = 28 // px (h-7)
 export const EVENT_BAR_HEIGHT = 24 // px (h-[24px])

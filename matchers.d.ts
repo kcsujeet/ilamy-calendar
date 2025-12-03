ec41a4e96dd5bfca239cd391f241f42dbc65ac94
Sujeet Kc <sujeetkc45@gmail.com>
2025-12-03 11:33:19 -0400

    refactor: simplify interface declarations in matchers.d.ts for improved readability

--- matchers.d.ts
+++ matchers.d.ts
@@ -2,12 +2,8 @@ import type { TestingLibraryMatchers } from '@testing-library/jest-dom/matchers'
 import type { Matchers, AsymmetricMatchers, expect } from 'bun:test'
 
 declare module 'bun:test' {
-  interface Matchers<T> extends TestingLibraryMatchers<
-    typeof expect.stringContaining,
-    T
-  > {}
-  interface AsymmetricMatchers extends TestingLibraryMatchers<
-    unknown,
-    unknown
-  > {}
+  interface Matchers<T>
+    extends TestingLibraryMatchers<typeof expect.stringContaining, T> {}
+  interface AsymmetricMatchers
+    extends TestingLibraryMatchers<unknown, unknown> {}
 }
